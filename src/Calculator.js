486c8075c5d7899deddb29807d6f5128a4b0a905
Jeff <jlgranof@icloud.com>
2020-09-16 12:31:00 -0500

    Phase 4: Creating input fileds - Complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,11 +10,23 @@ class Calculator extends React.Component {
     }
   }
 
+  handleFirstNum = e => {
+      this.setState({ num1: e.target.value });
+  }
+
+  handleSecondNum = e => {
+    this.setState({
+      num2 : e.target.value
+    })
+}
+
   render() {
     const {result, num1, num2} = this.state
     return (
       <div>
         <h1>Result: {result}</h1>
+        <input onChange={this.handleFirstNum} placeholder="First number" value={this.state.num1} />
+        <input onChange={this.handleSecondNum} placeholder="Second number" value={this.state.num2} />
       </div>
     );
   }
