2bb88944f8e5fdcf4b80f1b15b406ccc1e1dc7e0
Jeff <jlgranof@icloud.com>
2020-09-16 12:20:35 -0500

    Phae 2: Creating the Calculator component - Complete

---
+++ Calculator.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    // TODO: Initialize state
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Time for math!</h1>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
